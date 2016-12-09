97369523bde2e885a45846eb5b3f8917f2ef7014
Arne Hatlen <ashatlen@gmail.com>
2016-12-09 07:23:47 +0100

    Started on ninth

---
+++ Niende.fs
@@ -0,0 +1,27 @@
+﻿namespace Julekalender
+
+module Niende=
+
+    open System
+    open System.IO
+
+    let rec registerTranxations (accounts:Map<Guid, int>) (transactions:StreamReader)=
+        let transaction= transactions.ReadLine().Split(' ') |> Array.toList
+        match transaction with
+        | ["None";accName;amount] -> 
+            let toAccount= Guid.Parse accName
+            let currBalance= 
+                match (Map.tryFind toAccount accounts ) with 
+                | Some x -> x
+                | _ -> 0
+
+            transactions |> Map.add 
+        | ["toAccount";toAccount;amount] ->
+            
+
+
+        accounts
+
+    let transaksjonshistorie = 
+        let transaksjons= File.OpenText("Transaksjoner.txt")
+        registerTranxations Map.empty transaksjons
