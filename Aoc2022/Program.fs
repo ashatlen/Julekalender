638e8588a015f8727b07e0210f8d5dd9a97fedfc
Arne Hatlen <ashatlen@gmail.com>
2022-12-04 20:47:11 +0100

    Completed 4

--- Aoc2022/Program.fs
+++ Aoc2022/Program.fs
@@ -85,15 +85,86 @@ module Hoved =
         | (3,"Z") -> 6 + 1 // Win with Rock
         | _ -> 0
 
-
-    let andredesember = fun (puzzle: string list) game ->        
+    let andredesember = fun (puzzle: string list) (game: string -> string -> int) ->        
         puzzle 
         |> List.sumBy (fun g -> 
+
             let items = g.Split ' ' 
             let opp = items |> Array.head
             let resp = items |> Array.skip 1 |> Array.head
+
             game opp resp
             )
+
+
+    let splitEqual = fun (s: string) ->
+        let s1 = s.Substring (0, s.Length / 2)
+        let s2 = s.Substring (s.Length / 2)
+        (s1,s2) 
+
+    let rec IsInString = fun (s:string) (cs:char list) ->
+
+        if cs = List.empty then
+            None
+        else
+            let c::r = cs
+            if s.Contains(c) then
+                Some c
+            else
+                IsInString s r
+
+    let rec IsInStrings = fun (s1:string) (s2:string) (cs:char list) ->
+
+        if cs = List.empty then
+            None
+        else
+            let c::r = cs
+            if s1.Contains(c) && s2.Contains(c) then
+                Some c
+            else
+                IsInStrings s1 s2 r
+
+
+    let mapToPriority= fun x ->
+        match x with
+        | (Some c) when c > 'Z' -> (c |> int) - ('a' |> int) + 1
+        | (Some c) -> (c |> int) - ('A' |> int)  + 27
+
+    let tredjedesember = fun (puzzle: string list) ->
+
+        let findSingle = fun (s1: string, s2: string) ->
+                
+            s1 
+            |> Seq.toList
+            |> IsInString s2
+            |> mapToPriority
+
+        puzzle
+        |> List.map (fun rucksack -> 
+            rucksack 
+            |> splitEqual 
+            |> findSingle 
+            )
+        |> List.sum
+
+    let tredjedesember2 = fun (puzzle: string list) ->
+
+        let findCommon = fun (s1: string) (s2: string) (s3: string) ->                
+            s1 
+            |> Seq.toList
+            |> IsInStrings s2 s3
+
+        let parts = puzzle.Length / 3
+
+        puzzle
+        |> List.splitInto parts            
+        |> List.map  (fun g -> 
+            let a= g |> List.toArray
+            findCommon a[0] a[1] a[2]
+            |> mapToPriority
+            )
+        |> List.sum
+
 
     [<EntryPoint>]
     let main argv = 
@@ -102,9 +173,13 @@ module Hoved =
         // printf "1.desember 1 top elf %i\n" (forstedesember1 puzzle)
         // printf "1.desember 2 top 3 elfs %i\n" (forstedesember2 puzzle)
 
-        let puzzle= File.ReadAllLines(@"dec2.txt") |> List.ofSeq
-        printf "2.desember 1 Rock Paper Scissors %i\n" (andredesember puzzle game1)
-        printf "2.desember 1 Rock Paper Scissors %i\n" (andredesember puzzle game2)
+        // let puzzle= File.ReadAllLines(@"dec2.txt") |> List.ofSeq
+        // printf "2.desember 1 Rock Paper Scissors %i\n" (andredesember puzzle game1)
+        // printf "2.desember 2 Rock Paper Scissors %i\n" (andredesember puzzle game2)
+
+        let puzzle= File.ReadAllLines(@"dec3.txt") |> List.ofSeq
+        printf "3.desember 1 Rucksack %i\n" (tredjedesember puzzle)
+        printf "3.desember 2 Rucksack %i\n" (tredjedesember2 puzzle)
 
         Console.Read() |> ignore
         0 // return an integer exit code
